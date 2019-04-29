b7cee82a2451391550a91d460e014cc0009bcabe
Jide Omekam <jideomekam@jides-mbp.devices.brown.edu>
2019-04-29 15:28:01 -0400

    started LodePositiveTests

---
+++ test/LodePositiveTests.js
@@ -0,0 +1,26 @@
+const utils = require('./Utils')
+const Lode = artifacts.require('Lode')
+
+const amount = 100
+
+contract('LodePositiveTests', async function(accounts) {
+
+  beforeEach('Make fresh contract', async function() {
+    //let account[0] represent the owner. let account[5] represent the Game
+    lode = await Lode.new(accounts[0], {from: accounts[5]})
+  })
+
+  it('no resources should be minded', async function() {
+    let collection = await lode.collect.call()
+    assert.equal(collection[0].toNumber(), 0)
+
+    collection[1].forEach(function(item) {
+      assert.equal(item.toNumber(), 0)
+    });
+  })
+
+
+
+
+
+})
